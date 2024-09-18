9990a247c763144feba8a799ae6d15f4a731a6ff
olajide.o <olutolajide@gmail.com>
2024-09-18 09:54:46 +0200

    fix contract syntax clash

--- backend/contracts/DeedRepository.sol
+++ backend/contracts/DeedRepository.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 /**
 * @title Repository of ERC721 Deeds
@@ -10,37 +11,33 @@ import "./ERC721/ERC721.sol";
 * to the repository.
 */
 
-contract DeedRepository is ERC721 {
+contract DeedRepository is ERC721URIStorage {
   /**
-* @dev Created a DeedRepository with a name and symbol
-* @param _name string represents the name of the repository
-* @param _symbol string represents the symbol of the repository
-*/
-  constructor(string _name, string _symbol) ERC721(_name, _symbol) {}
+  * @dev Created a DeedRepository with a name and symbol
+  * @param _contractName string represents the name of the repository
+  * @param _contractSymbol string represents the symbol of the repository
+  */
+  constructor(string memory _contractName, string memory _contractSymbol) ERC721(_contractName, _contractSymbol) {}
 
-  function registerdDeed(uint256 _tokenId, string _uri) public {
+  function registerDeed(uint256 _tokenId, string memory _uri) public {
     _mint(msg.sender, _tokenId);
-    addDeedMetadata(_tokenId, _uri);
+    _setDeedMetadata(_tokenId, _uri);  // Now using internal function
     emit DeedRegistered(msg.sender, _tokenId);
   }
 
   /**
-  * @dev Public function to add metadata to a deed
+  * @dev Internal function to add metadata to a deed
   * @param _tokenId represents a specific deed
   * @param _uri text which describes the characteristics of a given deed
-  * @return whether the deed metadata was added to the repository
   */
-
-  function addDeedMetadata(uint _tokenId, string _uri) public returns(bool) {
-    _setTokenURI(_tokenId,_uri);
-    return true;
+  function _setDeedMetadata(uint _tokenId, string memory _uri) internal {
+    _setTokenURI(_tokenId, _uri);  // calling the internal function _setTokenURI
   }
 
   /**
-* @dev Event is triggered if deed/token is registered
-* @param _by address of the registrar
-* @param _tokenId uint256 represents a specific deed
-*/
-
-  event DeedRegistered(address _by, uint26 _tokenId)
+  * @dev Event is triggered if deed/token is registered
+  * @param _by address of the registrar
+  * @param _deedTokenId uint256 represents a specific deed
+  */
+  event DeedRegistered(address _by, uint256 _deedTokenId);
 }
